4fe5f54fffee643915e6b3ef8fe77fb5785df38d
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-02-25 11:56:52 +0530

    Created Player

---
+++ lib/components/player.dart
@@ -0,0 +1,29 @@
+
+import 'dart:ui';
+
+import 'package:tap_n_tap/game_controller.dart';
+
+class Player {
+
+  final GameController gameController;
+  int maxHealth;
+  int currentHealth;
+  bool isDead;
+
+  Player(this.gameController) {
+    maxHealth = currentHealth = 3000;
+    isDead = false;
+  }
+
+  void render(Canvas c) {
+    // TODO: no need for now
+  }
+
+  void update(double t) {
+    if (!isDead && currentHealth <= 0) {
+      isDead = true;
+      // reset game
+    }
+  }
+
+}
